d6cb66255745bd11005e637fc1e4caf253820432
Dmitry <234518@mail.ru>
2025-11-03 23:15:26 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const fs = require("fs");
+
+let buffer = fs.readFileSync("image.bmp");
+
+let signature = String.fromCharCode(buffer[0],buffer[1]);
+console.log(signature) //"BM"
+
+let offset = 2;
+let size = buffer.readUint32LE(offset);
+offset += 4;
+
+//reserved 4 bytes, must be zero
+offset += 4; 
+
+let pixelDataOffset = buffer.readUint32LE(offset);
+offset += 4;
+
+//BITMAPINFOHEADER
+let header = {}
+header.headerSize = buffer.readUInt32LE(offset);
+offset += 4;
+header.width = buffer.readUInt32LE(offset);
+offset += 4;
+header.height = buffer.readUInt32LE(offset);
+offset += 4;
+header.numberOfColorPlanes = buffer.readUint16LE(offset);
+offset += 2;
+header.bitsPerPixel = buffer.readUint16LE(offset);
+offset += 2;
+header.compression = buffer.readUint32LE(offset);
+offset += 4;
+header.sizeOfBitmapData = buffer.readUint32LE(offset);
+offset += 4;
+header.horizontalResolution = buffer.readUint32LE(offset);//pixels per meter
+offset += 4;
+header.verticalResolution = buffer.readUint32LE(offset);//pixels per meter
+offset += 4;
+header.numberOfColorsInPalette = buffer.readUint32LE(offset);
+offset += 4;
+header.numberOfImportantColors = buffer.readUint32LE(offset);
+offset += 4;
+console.log(header)
+
+
+
+function getOffset(x,y)
+{
+    let rowSize = Math.ceil(header.width*3/4)*4;
+    return pixelDataOffset + rowSize*(header.height - y - 1) + 3*x
+}
+
+function rect(x,y,width,height,R=0,G=0,B=0)
+{
+    for(let X=x; X<x+width; X++)
+    {
+        for(let Y=y; Y<y+height; Y++)
+        {
+            let i = getOffset(X,Y);
+            buffer.writeUint8(B,i);
+            buffer.writeUint8(G,i+1);
+            buffer.writeUint8(R,i+2);
+        }
+    }
+}
+
+rect(0,0,800,600,0,0,255);
+
+fs.writeFileSync("image.bmp",buffer)
