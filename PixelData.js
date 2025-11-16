ff10827744064d2f96cc2c8565edab7e0e9cfa0a
Dmitry <234518@mail.ru>
2025-11-16 18:10:22 +0300

    opacity

--- PixelData.js
+++ PixelData.js
@@ -39,16 +39,33 @@ class PixelData
         return {R,G,B};
     }
 
-    setPixel(x,y,R,G,B)
+    setPixel(x,y,R,G,B,opacity=1)
     {
         let X = Math.round(x);
         let Y = Math.round(y);
-        if(X < 0 || X > this.width - 1 || Y < 0 || Y > this.height - 1) return;
+        if(X < 0 || X > this.width - 1 || Y < 0 || Y > this.height - 1 || opacity <= 0 || opacity > 1) return;
 
         let offset = this.getOffset(X,Y);
-        this.buffer.writeUint8(B,offset);
-        this.buffer.writeUint8(G,offset+1);
-        this.buffer.writeUint8(R,offset+2);
+        if(opacity == 1)
+        {
+            this.buffer.writeUint8(B,offset);
+            this.buffer.writeUint8(G,offset+1);
+            this.buffer.writeUint8(R,offset+2);
+        }
+        else if(opacity > 0 && opacity < 1)
+        {
+            let prevB = this.buffer.readUint8(offset);
+            let prevG = this.buffer.readUint8(offset+1);
+            let prevR = this.buffer.readUint8(offset+2);
+
+            let b = Math.round((1-opacity)*prevB + opacity*B);
+            let g = Math.round((1-opacity)*prevG + opacity*G);
+            let r = Math.round((1-opacity)*prevR + opacity*R);
+
+            this.buffer.writeUint8(b,offset);
+            this.buffer.writeUint8(g,offset+1);
+            this.buffer.writeUint8(r,offset+2);
+        }
     }
 
     getContext()
